77372d4db8e67d59afc7cd5cb2df49184447f23e
Saksham Ayush <saksham.barca@gmail.com>
2022-11-09 22:16:29 -0600

    Added comments

--- src/js/overview-1.js
+++ src/js/overview-1.js
@@ -1,13 +1,16 @@
 choropleth = async () => {
-  let usData = await d3.json("https://unpkg.com/us-atlas@3/counties-10m.json");
+  //Adding the dataset
   let data = await d3.csv("Data/final_data.csv");
+  //Creating svg for the map
   let svg = d3.select("#map")
               .attr("margin-left","00px");
   let projection = d3.geoAlbersUsa();
   let path = d3.geoPath(projection);
+  //Setting colors for the scale
   let lowColor = '#f9f9f9';
   let highColor = '#bc2a66';
 
+  //Getting counts for each state
   temp = {}
   for(let i=0; i<data.length; i++)
   {
@@ -23,8 +26,10 @@ choropleth = async () => {
 
   let minValue = Number.POSITIVE_INFINITY;
   let maxValue = Number.NEGATIVE_INFINITY;
+  //Adding US map data
   let mapdata = await d3.json("Data/us-states.geojson");
   
+  //Matching state from the map data and our dataset
   for(let i=0;i<Object.keys(temp).length;i++)
   {
     let state = Object.keys(temp)[i];
@@ -48,8 +53,10 @@ choropleth = async () => {
       }
     }
   }
+  //To add color to the map
   let ramp = d3.scaleLinear().domain([minValue,maxValue]).range([lowColor,highColor]);
 
+  //Creating tooltip
   let tooltip = d3.select("body")
   .append("div")
   .style("position", "absolute")
@@ -63,6 +70,7 @@ choropleth = async () => {
   .style("padding", "15px")
   .text("a simple tooltip");
 
+  //Creating the map
   svg.selectAll("path")
      .data(mapdata.features)
      .enter()
@@ -82,49 +90,50 @@ choropleth = async () => {
         return tooltip.style("visibility", "hidden");
       });
 
-    let w = 140, h = 300;
+  //Adding legend
+  let w = 140, h = 300;
 
-		let key = d3.select("#map")
-                .append("svg")
-                .attr("width", w)
-                .attr("height", h)
-                .attr("class", "legend");
+  let key = d3.select("#map")
+              .append("svg")
+              .attr("width", w)
+              .attr("height", h)
+              .attr("class", "legend");
 
-		let legend = key.append("defs")
-                    .append("svg:linearGradient")
-                    .attr("id", "gradient")
-                    .attr("x1", "100%")
-                    .attr("y1", "0%")
-                    .attr("x2", "100%")
-                    .attr("y2", "100%")
-                    .attr("spreadMethod", "pad");
+  let legend = key.append("defs")
+                  .append("svg:linearGradient")
+                  .attr("id", "gradient")
+                  .attr("x1", "100%")
+                  .attr("y1", "0%")
+                  .attr("x2", "100%")
+                  .attr("y2", "100%")
+                  .attr("spreadMethod", "pad");
 
-		legend.append("stop")
-          .attr("offset", "0%")
-          .attr("stop-color", highColor)
-          .attr("stop-opacity", 1);
+	legend.append("stop")
+        .attr("offset", "0%")
+        .attr("stop-color", highColor)
+        .attr("stop-opacity", 1);
 			
-		legend.append("stop")
-          .attr("offset", "100%")
-          .attr("stop-color", lowColor)
-          .attr("stop-opacity", 1);
+	legend.append("stop")
+        .attr("offset", "100%")
+        .attr("stop-color", lowColor)
+        .attr("stop-opacity", 1);
 
-		key.append("rect")
-			 .attr("width", w - 100)
-			 .attr("height", h)
-			 .style("fill", "url(#gradient)")
-			 .attr("transform", "translate(0,10)");
+	key.append("rect")
+			.attr("width", w - 100)
+			.attr("height", h)
+			.style("fill", "url(#gradient)")
+			.attr("transform", "translate(0,10)");
 
-		let y = d3.scaleLinear()
-              .range([h, 0])
-              .domain([minValue, maxValue]);
+	let y = d3.scaleLinear()
+            .range([h, 0])
+            .domain([minValue, maxValue]);
 
-		let yAxis = d3.axisRight(y);
+	let yAxis = d3.axisRight(y);
 
-		key.append("g")
-			 .attr("class", "y axis")
-			 .attr("transform", "translate(41,10)")
-			 .call(yAxis);
+	key.append("g")
+			.attr("class", "y axis")
+			.attr("transform", "translate(41,10)")
+			.call(yAxis);
 
 }
 choropleth()
